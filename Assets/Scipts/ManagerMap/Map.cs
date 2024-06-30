dcbab636e4067892fb1dd2ebaf6d2830cc54631b
Si Tien <duongsitien123@gmail.com>
2024-06-30 15:05:15 +0700

    fix error

--- Assets/Scipts/ManagerMap/Map.cs
+++ Assets/Scipts/ManagerMap/Map.cs
@@ -1,50 +1,66 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using static UnityEditor.Progress;
 
-public class MapOne : MonoBehaviour
+
+public class Map : MonoBehaviour
 {
+
     [Header("Enemy Object")]
     [SerializeField] GameObject enemy1;
     [SerializeField] GameObject enemy2;
     [SerializeField] GameObject enemy3;
-    [SerializeField] GameObject boss; 
-    
-    [SerializeField] GameObject item;
+    [SerializeField] GameObject enemy4; 
+    [SerializeField] GameObject enemy5; 
+    [SerializeField] GameObject enemy6;
+    [SerializeField] GameObject boss;
+
+    [Header("Item Object")]
+    [SerializeField] GameObject item1;  
+    [SerializeField] GameObject item2;  
+    [SerializeField] GameObject item3;
 
     [Header("Time Delay")]
     [SerializeField] float time1;
     [SerializeField] float time2;
     [SerializeField] float time3;
-    [SerializeField] float time4; 
+    [SerializeField] float time4;
     [SerializeField] float time5;
+    [SerializeField] float time6;
+     [SerializeField] float timeBoss;
+
+
 
     [Header("UI Elements")]
-    [SerializeField] GameObject instructionPanel; 
+    [SerializeField] GameObject instructionPanel;
 
     [SerializeField] Button playButton;
     private bool gameStarted = false;
 
-  
+    // Start is called before the first frame update
     private void Awake()
     {
-      
+
         enemy1.SetActive(false);
         enemy2.SetActive(false);
         enemy3.SetActive(false);
+        enemy4.SetActive(false);
+        enemy5.SetActive(false);
+        enemy6.SetActive(false);
         boss.SetActive(false);
-        item.SetActive(false);
-       
+        item1.SetActive(false);
+        item2.SetActive(false);
+        item3.SetActive(false);
 
-      
+
+
         // Hiển thị thông báo nhiệm vụ
         instructionPanel.SetActive(true);
 
         playButton.onClick.AddListener(OnPlayButtonClick);
     }
-
-  
 
     public void OnPlayButtonClick()
     {
@@ -53,7 +69,7 @@ public class MapOne : MonoBehaviour
         {
             gameStarted = true;
             instructionPanel.SetActive(false);
-           
+
             StartCoroutine(StartMission());
         }
     }
@@ -66,20 +82,44 @@ public class MapOne : MonoBehaviour
         yield return new WaitForSeconds(time2);
         enemy1.SetActive(false);
         enemy2.SetActive(true);
+        item1.SetActive(true) ;
 
-        //yield return new WaitForSeconds(time5);
-        item.SetActive(true);
+      
 
         yield return new WaitForSeconds(time3);
         enemy2.SetActive(false);
         enemy3.SetActive(true);
-        item.SetActive(false );
+        item1.SetActive(false);
+
 
         yield return new WaitForSeconds(time4);
         enemy3.SetActive(false);
-        boss.SetActive(true);
+        enemy4.SetActive(true);
 
-        yield return new WaitForSeconds(20f);
-       
+         yield return new WaitForSeconds(time5);
+        enemy4.SetActive(false);
+        enemy5.SetActive(true);
+        item2.SetActive(true) ;
+
+        yield return new WaitForSeconds(time6);
+        enemy5.SetActive(false);
+        enemy6.SetActive(true);
+        item2.SetActive(false); 
+        
+        yield return new WaitForSeconds(timeBoss);
+        enemy6.SetActive(false);
+        boss.SetActive(true);
+        item3.SetActive(true);
+
+        
+
+        yield return new WaitForSeconds(50f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
